2e1b1654e9d438ac2881d9e24139f4b1efd18c5a
eltoncox <elton.unix@gmail.com>
2025-03-20 20:10:45 -0300

    Validation, e migration com flyway.

--- src/main/java/com/elton/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/elton/med/voll/api/controller/MedicoController.java
@@ -4,7 +4,9 @@ import com.elton.med.voll.api.endereco.Endereco;
 import com.elton.med.voll.api.medico.DadosCadastroMedico;
 import com.elton.med.voll.api.medico.Medico;
 import com.elton.med.voll.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,8 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados ) {
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados ) {
         repository.save(new Medico(dados));
     }
 }
